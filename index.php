8c132d683a2b00cc66590f61e726a869183001ea
Colin Chambers <c.chambers@open.ac.uk>
2011-04-13 14:26:15 +0100

    added index file to aid navigation

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Moodle Development: Shortcuts</title>
+	</head>
+	<body>
+		<h1>CC5983: VLEDEV2 Shortcuts</h1>
+
+		<h2>Topics</h2>
+
+		<h3>Offline Moodle</h3>
+		<h4>Snapshots</h4>
+		<ul>
+			<li>
+				<a href="offline moodle/snapshots/user interface/v0.5/index.html">User interface</a>
+				<ul>
+					<li><a href="offline moodle/snapshots/user interface/v0.5/v0.5/index.html">v0.5</a></li>
+					<li><a href="offline moodle/snapshots/user interface/v0.5/v0.4/index.html">v0.4</a></li>
+					<li><a href="offline moodle/snapshots/user interface/v0.5/v0.3/index.html">v0.3</a></li>
+					<li><a href="offline moodle/snapshots/user interface/v0.5/v0.2/index.html">v0.2</a></li>
+					<li><a href="offline moodle/snapshots/user interface/v0.5/v0.1/index.html">v0.1</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="offline moodle/snapshots/v0.1/index.html">v0.1</a>
+			</li>
+		</ul>
+	</body>
+</html>
