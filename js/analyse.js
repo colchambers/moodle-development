5800b27914e1246bdf4dc16870c555b53bfc9fe1
colchambers <colchambers@gmail.com>
2011-04-15 15:42:14 +0000

    section toggle works on toggle.html and essay-question.html. Add debugging helper methods

--- js/analyse.js
+++ js/analyse.js
@@ -2,7 +2,7 @@
  var Out = {
     initialiseConsole: function (){
      // Create console
-        // Create a YUI instance and request the console module and its dependencies
+       /* // Create a YUI instance and request the console module and its dependencies
         YUI({ filter: 'raw' }).use("console", "console-filters", "dd-plugin", function (Y) {
          
             console = new Y.Console({
@@ -13,18 +13,40 @@
         			clear : 'Flush',
         			collapse : 'Shrink',
         			expand : 'Grow'
-        		},
-                logSouorce: Y.Global
+        		}
         	}).plug(Y.Plugin.ConsoleFilters)
         	  .plug(Y.Plugin.Drag, { handles: ['.yui3-console-hd'] })
         	  .render();
         	 
         });
-     },
-     debug: function (message, filter){
+        */
+    },
+    append: function (message, filter){
         YUI({ filter: 'raw' }).use("node", function (Y) {
                 Y.log(message, filter);
         });
-    }
+    
+    },
+     iterate: function (variable, message, full){
+         message == message?message: 'Variable = ';
+             
+         if(variable.length){
+             for( x=0;x<variable.length;x++ ){
+            	this.append( message + " ( " + x + " ) = " + variable[ x ] )
+    		}
+         }
+         
+         var method;
+         for( method in variable ){
+    		if( full ){
+				this.append( method + " = " + this.getObjectMethod( variable, method ));
+			}
+			else{
+				this.append( method);
+			}
+		}
+     },
+     
+     getObjectMethod: function ( variable, method ){ return variable[ method ]; }
     
 }
---
+++ js/display.js
@@ -0,0 +1,34 @@
+var Display = {
+    toggle: function (id, style){
+        YUI().use('node', function(Y) {
+            var node = Y.one(id);
+            style = style?style:'block';
+            if(style == node.getStyle('display')){        
+               if(!node.hide){// is new hide method implemented
+                    node.setStyle('display', 'none');
+                    return;
+                }
+                
+                node.hide();
+                return;
+            }
+            // show element
+            if(!node.show){// is new show method implemented    
+                node.setStyle('display', style);
+                return;
+            }
+                
+            node.show();
+        })
+    },
+    
+    isVisible: function(id){
+        
+        YUI().use('node', function(Y) {
+            visible = Y.one(id).getStyle('display')=='none'?false:true;
+        })
+        
+        return visible;
+      
+    }
+}
